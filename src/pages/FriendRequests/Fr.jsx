067de1f1581cb0f17bb5d588d92debdeb3852973
isthatdhruv <isthatdhruvv@gmail.com>
2024-12-05 00:15:39 +0530

    frontend v1 for friend requests page

--- src/pages/FriendRequests/Fr.jsx
+++ src/pages/FriendRequests/Fr.jsx
@@ -7,15 +7,43 @@ const Fr = () => {
     <div className="fr">
       <div className="fr-box">
         <h1>Friend Requests</h1>
-        <hr/>
+        <hr />
         <div className="fr-list">
-          <div className="friends add-user">
-            <img src={assets.avatar_icon} alt="" />
-            <p>username</p>
-            <div className="buttons">
-                <img src={assets.tick} alt=""/>
-                <img src={assets.cross} alt="" />
-            </div>
+            <div className="fr-item">
+                <img src={assets.avatar_icon} alt="user1" />
+                <h2>John Doe</h2>
+                <p>bio</p>
+                <div className="friend-btn">
+                    <button className="fr-btn1">Accept</button>
+                    <button className="fr-btn2">Reject</button>
+                </div>
+          </div>
+          <div className="fr-item">
+                <img src={assets.avatar_icon} alt="user1" />
+                <h2>John Doe</h2>
+                <p>bio</p>
+                <div className="friend-btn">
+                    <button className="fr-btn1">Accept</button>
+                    <button className="fr-btn2">Reject</button>
+                </div>
+          </div>
+          <div className="fr-item">
+                <img src={assets.avatar_icon} alt="user1" />
+                <h2>John Doe</h2>
+                <p>bio</p>
+                <div className="friend-btn">
+                    <button className="fr-btn1">Accept</button>
+                    <button className="fr-btn2">Reject</button>
+                </div>
+          </div>
+          <div className="fr-item">
+                <img src={assets.avatar_icon} alt="user1" />
+                <h2>John Doe</h2>
+                <p>bio</p>
+                <div className="friend-btn">
+                    <button className="fr-btn1">Accept</button>
+                    <button className="fr-btn2">Reject</button>
+                </div>
           </div>
         </div>
       </div>
