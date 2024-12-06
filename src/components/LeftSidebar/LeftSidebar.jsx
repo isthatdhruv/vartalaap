09c678e3426ce25a805a6df50c22f7e9677a4d8b
isthatdhruv <isthatdhruvv@gmail.com>
2024-12-06 12:31:34 +0530

    another chota mota change and linked cliploader to login and create account button in login page

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -6,6 +6,7 @@ import {
   arrayUnion,
   collection,
   doc,
+  getDoc,
   getDocs,
   query,
   serverTimestamp,
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,21 +3,35 @@ import './Login.css';
 import assets from '../../assets/assets';
 import { useState } from "react";
 import  {signup , login ,resetPass}  from "../../config/firebase";
+import { toast } from "react-toastify";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Login = () => {
     const[currentState, setCurrentState] = useState('Sign Up');
     const[userName, setUserName] = useState('');
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
+    const[loading, setLoading] = useState(false);
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        if (currentState === 'Sign Up') {
-            signup(userName, email, password);
+        try {
+            if (currentState === 'Sign Up') {
+                setLoading(true);
+                signup(userName, email, password);
+            }
+            else{
+                setLoading(true);
+                login(email, password);
+            }
+        } catch (error) {
+            toast.error(error.message);
+            
+        }finally{
+            setLoading(false);
         }
-        else{
-            login(email, password);
-        }
+        
+        
     }
 
     return (
@@ -31,7 +45,7 @@ const Login = () => {
                 
                 <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder="Password" className="form-input" required/>
                 
-                <button>{currentState === "Sign Up" ? "Create Account":"Login"}</button>
+                <button onClick={onSubmitHandler} disabled={loading}>{loading ? (<ClipLoader color="white" loading={loading} size={30} margin={5}/>) : (currentState === "Sign Up" ? "Create Account" : "Login")}</button>
                 
                 <div className="login-term">
                     <input type="checkbox" name="term" id="term" required/>
